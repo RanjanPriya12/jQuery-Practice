ad91bbf32f1a6dc1a7b738f2fbf19257eba08b78
Priya Ranjan <97494807+RanjanPriya12@users.noreply.github.com>
2023-06-18 03:17:01 +0530

    cart page design

---
+++ Jquery/cart.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+  const cartItems = JSON.parse(localStorage.getItem("cartItems"));
+  console.log(cartItems);
+  const table = `<table>
+                    <thead>
+                        <tr>
+                            <th>Food Image</th>
+                            <th>Description</th>
+                            <th>Quantity</th>
+                            <th>Price</th>
+                            <th>Remove</th>
+                        </tr>
+                    </thead>
+                    <tbody id="itemCard"></tbody>
+              </table>`;
+              $(".itemsSection").append(table);
+  $.each(cartItems, (index, item) => {
+    const cartItem = `<tr key="${index}">
+    <td><div><img src="${item.image}" alt="image"/></div></td>
+    <td>${item.title}</td>
+    <td><button id="deleteQuantity">-</button>1<button id="addQuantity">+</button></td>
+    <td>${item.price} Rs.</td>
+    <td><button id="removeButton">remove</button></td>
+</tr>`;
+    $("#itemCard").append(cartItem);
+  });
+});
